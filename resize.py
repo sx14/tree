733b1a03d0ab1b454e3cea42484ba00f3f1a45c5
sunx <1059363093@qq.com>
2019-07-02 20:18:05 +0800

    ..

--- resize.py
+++ resize.py
@@ -11,6 +11,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description='TreeMeasure(resize) V1.1')
     parser.add_argument('-i', '--input',    help='Please provide input image path.',    required=True)
     parser.add_argument('-o', '--output',   help='Please provide output image path.',   required=True)
+    parser.add_argument('-W', '--width', help='Resize width.', type=int)
+    parser.add_argument('-H', '--height', help='Resize width.', type=int)
     args = parser.parse_args()
     return args
 
@@ -20,10 +22,17 @@ if __name__ == '__main__':
         args = parse_args()
         input_path = args.input
         output_path = args.output
+        width = args.width
+        height = args.height
+
+        if width is None:
+            width = config.SAVE_WIDTH
+        if height is None:
+            height = config.SAVE_HEIGHT
 
         if os.path.exists(input_path):
             im = cv2.imread(input_path)
-            resize_size = (config.SAVE_WIDTH, config.SAVE_HEIGHT)
+            resize_size = (width, height)
             im_resized = cv2.resize(im, resize_size)
             cv2.imwrite(output_path, im_resized)
             print(0)
