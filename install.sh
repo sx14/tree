9997d375c1736a8aa68840853e6b907e0abc03da
sunx <1059363093@qq.com>
2019-07-08 19:06:23 +0800

    install.sh readme.md

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
-
+echo "Preparing virtual environment ..."
 virtualenv -p python2.7 env
+echo "Preparing virtual environment ... success!"
 source env/bin/activate
+echo "Activate virtual environment ... success!"
+echo "Installing requirements for TreeMeasure-python-part (this may take a long time) ..."
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+echo "Installing requirements for TreeMeasure-python-part ... success!"
